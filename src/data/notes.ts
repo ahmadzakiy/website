7670a15b8ebe45d3152709c7fb5b11723673f06c
Ahmad Zakiy <ahmad.zakiy@mekari.com>
2025-07-23 13:54:44 +0700

    update notes

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -31,7 +31,6 @@ export const notesData: NotesData = {
       href: "https://oreilly.com/library/view/what-is-the/9781492072973/ch01.html",
       title: "What is Token Economy?",
     },
-
     {
       href: "https://ethereum.org/en/web3/",
       title: "Introduction to Web3",
@@ -61,6 +60,11 @@ export const notesData: NotesData = {
       title: "CSS Utility Classes and Separation of Concerns",
     },
     {
+      href: "https://antfu.me/posts/reimagine-atomic-css",
+      title: "Reimagine Atomic CSS",
+    },
+
+    {
       href: "https://www.nngroup.com/articles/ten-usability-heuristics/",
       title: "Ten Usability Heuristics for User Interface Design",
     },
@@ -77,10 +81,13 @@ export const notesData: NotesData = {
       href: "https://svelte.dev/blog/frameworks-without-the-framework",
       title: "Frameworks without the Framework",
     },
-
     {
       href: "https://www.jantakacs.com/the-story-of-figma-living-long-enough-as-a-hero-to-become-a-villain",
       title: "The Story of Figma",
+    },
+    {
+      href: "https://madebyevan.com/figma/webassembly-cut-figmas-load-time-by-3x/",
+      title: "WebAssembly Cut Figma's Load Time by 3x",
     },
     {
       href: "https://dev.to/emmabostian/ux-engineering-3hem",
