451238594ee37a3ffd0b35e542e29cdc79dc00ce
Ahmad Zakiy <in.zakiy@gmail.com>
2026-02-27 19:48:53 +0700

    feat: update configuration for Cloudflare integration and adjust package scripts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 }
 
 export default nextConfig
+
+// Enable calling `getCloudflareContext()` in `next dev`.
+// See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare"
+
+initOpenNextCloudflareForDev()
